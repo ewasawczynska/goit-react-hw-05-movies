00d4b1ac91eae27e58eab5b7c699d6349a742df9
ewasawczynska <sawczynska.e@gmail.com>
2023-11-09 21:46:41 +0100

    Start the task

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,5 @@
+import { StyledNavigation } from './Navigation.styled';
+
+export const Navigation = () => {
+  return <StyledNavigation>Home</StyledNavigation>;
+};
---
+++ src/components/Navigation/Navigation.styled.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledNavigation = styled.header`
+  background-color: #fff;
+`;
